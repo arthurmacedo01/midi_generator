d70ccb419132f4bcf489f03433cadb3633241a4c
Arthur_Macedo <arthurmacedo01@gmail.com>
2022-12-23 15:42:40 -0300

    add: esp_timer high resolution

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
 
+esp_timer_handle_t timer_handle;
+
+static void periodic_timer_callback(void* arg){
+    printf("se passaram 5 ms!\n");
+}
+
+void timer_setup(){
+    const esp_timer_create_args_t periodic_timer_args = {
+        .callback = &periodic_timer_callback,
+        .name = "periodic"
+    };
+    esp_timer_create(&periodic_timer_args, &timer_handle);
+}
 
 void app_main() {
-    printf("Hello World!!\n");
+    timer_setup();
+    esp_timer_start_periodic(timer_handle, (uint64_t) 5000);
 }
