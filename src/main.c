52f7557a534d719ad9d045b6623d96851a9da601
Arthur_Macedo <arthurmacedo01@gmail.com>
2023-01-11 18:31:49 -0300

    add: encapsulate timer into c file

--- src/main.c
+++ src/main.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
-#include "freertos/FreeRTOS.h"
+#include "timer.h"
 
-esp_timer_handle_t timer_handle;
-
-static void periodic_timer_callback(void* arg){
-    printf("se passaram 5 ms!\n");
+void test(void *arg)
+{
+	printf("agora 500ms!\n");
+	return;
 }
 
-void timer_setup(){
-    const esp_timer_create_args_t periodic_timer_args = {
-        .callback = &periodic_timer_callback,
-        .name = "periodic"
-    };
-    esp_timer_create(&periodic_timer_args, &timer_handle);
-}
-
-void app_main() {
-    timer_setup();
-    esp_timer_start_periodic(timer_handle, (uint64_t) 5000);
+void app_main()
+{
+	timer_init(&test, (uint64_t)500000);
 }
