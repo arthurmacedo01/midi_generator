e19e678235298c9821c1a044a0ce102d06c62408
Arthur_Macedo <arthurmacedo01@gmail.com>
2023-01-12 00:05:31 -0300

    add: queue to the timer callback function

--- src/main.c
+++ src/main.c
@@ -3,9 +3,14 @@
 #include "sensor.h"
 #include "midi.h"
 
+QueueHandle_t xQueue = NULL;
+static TaskHandle_t xTaskExample = NULL;
+
 void app_main()
 {
 	sensor_init();
 	midi_setup();
-	timer_init(&readSensor, (uint64_t)300);
+	xQueue = xQueueCreate(10, sizeof(int));
+	timer_init(&readSensor, (void *)&xQueue, (uint64_t)300);
+	xTaskCreate(task1, "task", 1024 * 2, NULL, 5, &xTaskExample);
 }
--- src/timer.h
+++ src/timer.h
@@ -1,5 +1,5 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 
-esp_timer_handle_t timer_setup(void *arg);
-void timer_init(void *periodic_timer_callback, uint64_t interval);
+esp_timer_handle_t timer_setup(void *periodic_timer_callback, void *arg);
+void timer_init(void *periodic_timer_callback, void *arg, uint64_t interval);
